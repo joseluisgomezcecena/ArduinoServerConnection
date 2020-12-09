559e986db43dbcef315d691182c049f18fddd4d7
Martin Espericueta <62226552+MarespTech@users.noreply.github.com>
2020-12-08 16:31:58 -0800

    Add button
    Add button signal that when it is press send the get

--- ServerConnection.ino
+++ ServerConnection.ino
@@ -1,22 +1,20 @@
 /*
   Web client
-
  This sketch connects to a website (http://www.google.com)
  using an Arduino Wiznet Ethernet shield.
-
  Circuit:
  * Ethernet shield attached to pins 10, 11, 12, 13
-
  created 18 Dec 2009
  by David A. Mellis
  modified 9 Apr 2012
  modified 12/1/2020 by joseluisgomezcecena
  by Tom Igoe, based on work by Adrian McEwen
-
  */
 
 #include <SPI.h>
 #include <Ethernet.h>
+
+const int boton = 9;
 
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
@@ -43,6 +41,9 @@ unsigned long byteCount = 0;
 bool printWebData = true;  // set to false for better speed measurement
 
 void setup() {
+  pinMode(boton, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
   // You can use Ethernet.init(pin) to configure the CS pin
     Ethernet.init(10);  // Most Arduino shields
   //Ethernet.init(5);   // MKR ETH shield
@@ -82,67 +83,81 @@ void setup() {
   Serial.print("connecting to ");
   Serial.print(server);
   Serial.println("...");
-
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 80)== 1) {
-    Serial.print("connected to ");
-    Serial.println(client.remoteIP());
-    // Make a HTTP request:
-    client.println("GET /arduino/arduino.php?number=2 HTTP/1.1");
-    client.println("Host: mxmtsvrandon1");
-    client.println("Connection: close");
-    client.println();
-
-    /*
-    client.println("/arduino/arduino.php?number=1 HTTP/1.1");
-    client.println("Host: http://mxmtsvrandon1");
-    client.println("Connection: close");
-    client.println();
-    */
-
-   
-    
-  } else {
-    // if you didn't get a connection to the server:
-    Serial.println("connection failed");
-  }
-  beginMicros = micros();
 }
 
 void loop() {
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  int len = client.available();
-  if (len > 0) {
-    byte buffer[80];
-    if (len > 80) len = 80;
-    client.read(buffer, len);
-    if (printWebData) {
-      Serial.write(buffer, len); // show in the serial monitor (slows some boards)
+
+  valor = digitalRead(boton);
+  if(valor == HIGH) {
+    digitalWrite(LED_BUILTIN, HIGH);
+
+
+        // if you get a connection, report back via serial:
+    if (client.connect(server, 80)== 1) {
+      Serial.print("connected to ");
+      Serial.println(client.remoteIP());
+      // Make a HTTP request:
+      client.println("GET /arduino/arduino.php?number=2 HTTP/1.1");
+      client.println("Host: mxmtsvrandon1");
+      client.println("Connection: close");
+      client.println();
+  
+      /*
+      client.println("/arduino/arduino.php?number=1 HTTP/1.1");
+      client.println("Host: http://mxmtsvrandon1");
+      client.println("Connection: close");
+      client.println();
+      */
+  
+     
+      
+    } else {
+      // if you didn't get a connection to the server:
+      Serial.println("connection failed");
     }
-    byteCount = byteCount + len;
+    beginMicros = micros();
+    // if there are incoming bytes available
+    // from the server, read them and print them:
+    int len = client.available();
+    if (len > 0) {
+      byte buffer[80];
+      if (len > 80) len = 80;
+      client.read(buffer, len);
+      if (printWebData) {
+        Serial.write(buffer, len); // show in the serial monitor (slows some boards)
+      }
+      byteCount = byteCount + len;
+    }
+  
+    // if the server's disconnected, stop the client:
+    if (!client.connected()) {
+      endMicros = micros();
+      Serial.println();
+      Serial.println("disconnecting.");
+      client.stop();
+      Serial.print("Received ");
+      Serial.print(byteCount);
+      Serial.print(" bytes in ");
+      float seconds = (float)(endMicros - beginMicros) / 1000000.0;
+      Serial.print(seconds, 4);
+      float rate = (float)byteCount / seconds / 1000.0;
+      Serial.print(", rate = ");
+      Serial.print(rate);
+      Serial.print(" kbytes/second");
+      Serial.println();
+  
+      // do nothing forevermore:
+      while (true) {
+        delay(1);
+      }
+    }
+
+
+
+    
   }
-
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    endMicros = micros();
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
-    Serial.print("Received ");
-    Serial.print(byteCount);
-    Serial.print(" bytes in ");
-    float seconds = (float)(endMicros - beginMicros) / 1000000.0;
-    Serial.print(seconds, 4);
-    float rate = (float)byteCount / seconds / 1000.0;
-    Serial.print(", rate = ");
-    Serial.print(rate);
-    Serial.print(" kbytes/second");
-    Serial.println();
-
-    // do nothing forevermore:
-    while (true) {
-      delay(1);
-    }
+  else
+  {
+    digitalWrite(LED_BUILTIN, LOW);
   }
 }
